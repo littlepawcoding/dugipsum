0931825fb7b9b5a15a22c942682467c105824f9d
littlepawcoding <littlepawcoding@gmail.com>
2020-02-22 19:48:01 +0000

    updates to button function

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+let textTest1 = "Dug ipsum test text 1...";
+
 // function for button to display text
 function displayText() {
-  document.getElementById("text").innerHTML = "Dug ipsum text...";
+  document.getElementById("text").innerHTML = textTest1;
 }
