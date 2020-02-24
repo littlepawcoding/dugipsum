43d83b72e9d02a6ec37553911efd80f0ed3f1837
littlepawcoding <littlepawcoding@gmail.com>
2020-02-24 14:40:10 +0000

    working on connecting select and button - updating current changes

--- script.js
+++ script.js
@@ -7,7 +7,35 @@ const ipsumText = [
   "text 5..."
 ];
 
-// function for button to display text & randomise array
+// method for button click
+document.getElementById("button").addEventListener("click", displayText);
+
+// function to display text & randomise array
 function displayText() {
   text.innerHTML = ipsumText[Math.floor(Math.random() * ipsumText.length)];
+}
+
+// // function for total of paragraphs selection
+// document
+//   .getElementById("totalParagraphs")
+//   .addEventListener("change", totalParagraphs);
+
+// function totalParagraphs() {
+//   text.innerHTML = ipsumText;
+// }
+
+function dropdown() {
+  displayText();
+  let emptyString = "";
+  for (
+    let i = 0;
+    i <
+    document.select[0].totalParagraphs.options[
+      document.select[0].totalParagraphs.selectedIndex
+    ].value;
+    i++
+  ) {
+    emptyString += "<p>" + ipsumText[i] + "<" + "/p>";
+  }
+  document.getElementById("text").innerHTML = ipsumText;
 }
