9c03370d36b2257d55ab704f637c868a7565b270
littlepawcoding <littlepawcoding@gmail.com>
2020-02-25 11:49:07 +0000

    tried using switch statement to display dropdown value depending on selection - not working but updated changes

--- script.js
+++ script.js
@@ -7,36 +7,41 @@ const ipsumText = [
   "text 5..."
 ];
 
-let paragraphTotal = document.getElementById("totalParagraphs");
-let value = paragraphTotal[paragraphTotal.selectedIndex].value;
+let drop = document.getElementById("totalParagraphs").value;
 
-if (value === "1") {
-  console.log("hello 1");
+function dropdownChange() {
+  if (drop === "1") {
+    console.log("hello one");
+  } else if (drop == "2") {
+    console.log("hello 2");
+  } else {
+    console.log("three!!");
+  }
 }
 
-// function for dropdown to trigger array
-function dropdownChange(value) {
-  showSelected();
-}
+// // function for dropdown to trigger array
+// function dropdownChange(value) {
+//   showSelected();
+// }
 
-// function to display dropdown selection
-function showSelected() {
-  showRandom();
-  let paragraphTotal = document.getElementById("totalParagraphs");
-  let value = paragraphTotal[paragraphTotal.selectedIndex].value;
+// // function to display dropdown selection
+// function showSelected() {
+//   showRandom();
+//   let paragraphTotal = document.getElementById("totalParagraphs");
+//   let value = paragraphTotal[paragraphTotal.selectedIndex].value;
 
-  document.getElementById("displayText").innerHTML = value;
-}
-// method for button click
-document.getElementById("button").addEventListener("click", showSelected);
+//   document.getElementById("displayText").innerHTML = value;
+// }
+// // method for button click
+// document.getElementById("button").addEventListener("click", showSelected);
 
-// funtion to display dummy text & randomise
-// should random be in a separate function?
-let i = 0;
+// // funtion to display dummy text & randomise
+// // should random be in a separate function?
+// let i = 0;
 
-function showRandom() {
-  document.getElementById("displayRandom").innerHTML =
-    ipsumText[Math.floor(Math.random() * ipsumText.length)];
-  if (i == ipsumText.length) i = 0;
-}
-document.getElementById("button2").addEventListener("click", showRandom);
+// function showRandom() {
+//   document.getElementById("displayRandom").innerHTML =
+//     ipsumText[Math.floor(Math.random() * ipsumText.length)];
+//   if (i == ipsumText.length) i = 0;
+// }
+// document.getElementById("button2").addEventListener("click", showRandom);
