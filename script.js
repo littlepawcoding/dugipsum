c70b6effe9b4ce1a5345264cb4cd53bb387e1498
littlepawcoding <littlepawcoding@gmail.com>
2020-02-27 20:14:25 +0000

    following along with an example - changes updated

--- script.js
+++ script.js
@@ -1,51 +1,76 @@
 // dummy text
-const ipsumText = [
-  "text 1...",
-  "text 2...",
-  "text 3...",
-  "text 4...",
-  "text 5..."
-];
+let dugText = ["text 1...", "text 2...", "text 3...", "text 4...", "text 5..."];
 
-function ifSelection() {
-  let text;
-  let selection = document.getElementById("totalParagraphs").value;
+let start = ["Dug ipsum dolor "];
 
-  switch (selection) {
-    case "1":
-      text = "One";
-      break;
-    case "2":
-      text = "Two";
-    default:
-      text = "Select a number";
-  }
-  document.getElementById("displayText").innerHTML = text;
+function dugIpsum(dugText, start) {
+  this.dugText = dugText;
+  this.start = start;
+  this.startWith = true;
+  this.list;
+  this.startWith = document.getElementById("startDugIpsum");
+  this.fetchButton = document.getElementById("button");
+  this.lipsumParagraphsNum = document.getElementById("displayTheText");
+  this.displayRandom = document.getElementById("displayRandom");
+  this.numberOfParagraphs = 3;
+  this.maxNumberOfParagraphs = 10;
 }
 
-// // function for dropdown to trigger array
-// function dropdownChange(value) {
-//   showSelected();
-// }
+// not sure what this does yet
+dugIpsum.prototype.init = function() {
+  this.generateLipsumLister();
+};
 
-// // function to display dropdown selection
-// function showSelected() {
-//   showRandom();
-//   let paragraphTotal = document.getElementById("totalParagraphs");
-//   let value = paragraphTotal[paragraphTotal.selectedIndex].value;
+// creates list of words from dugText, if this.startWith add 1st line
+dugIpsum.prototype.initList = function() {
+  this.list = null;
+  let list;
 
-//   document.getElementById("displayText").innerHTML = value;
-// }
-// // method for button click
-// document.getElementById("button").addEventListener("click", showSelected);
+  // if checkbox is selected display Dug ipsum dolor...
+  if (this.startWith.checked == true) {
+    // slice dugText open to put start text in
+    // I want it to go at the beginning..
+    list = this.dugText.slice();
 
-// // funtion to display dummy text & randomise
-// // should random be in a separate function?
-// let i = 0;
+    // push in Dug ipsum dolor...
+    for (let i = 0; i < this.start.length; i++) {
+      list.push(this.start[i]);
+    }
+    this.list = list;
+  } else {
+    list = this.clean.slice();
+    this.list = list;
+  }
+};
 
-// function showRandom() {
-//   document.getElementById("displayRandom").innerHTML =
-//     ipsumText[Math.floor(Math.random() * ipsumText.length)];
-//   if (i == ipsumText.length) i = 0;
-// }
-// document.getElementById("button2").addEventListener("click", showRandom);
+dugIpsum.prototype.generateLipsumLister = function() {
+  let lipsum = this;
+  this.fetchButton.addEventListener("click", function() {
+    lipsum.initList();
+    lipsum.generateLipsum();
+  });
+
+  // increments number of paragraphs
+  document
+    .getElementById("totalParagraphs")
+    .addEventListener("click", function() {
+      if (lipsum.numberOfParagraphs < lipsum.maxNumberOfParagraphs) {
+        lipsum.numberOfParagraphs += 1;
+        lipsum.lipsumParagraphsNum.value =
+          lipsum.numberOfParagraphs + " Paragraphs";
+      }
+    });
+  // decrements number of paragraphs
+};
+
+// generates the lipsum paragraphs
+dugIpsum.prototype.generateLipsum = function() {
+  document.getElementById("displayRandom").style.display = "block";
+  let finalString = "";
+
+  for (let i = 0; i < this.numberOfParagraphs; i++) {
+    let paragraph = this.generateSentences();
+    finalString += "<p>" + paragraph + "</p>";
+  }
+  this.displayRandom.innerHTML - finalString;
+};
